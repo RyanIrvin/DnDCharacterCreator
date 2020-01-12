ef795502ffa242a3dde081161b3669a466bf831b
RyanIrvin <40035854+RyanIrvin@users.noreply.github.com>
2020-01-12 17:06:52 -0500

    Add Dwarf race

---
+++ DnDCharacterCreator/Model/Races/Dwarf.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDCharacterCreator.Model.Races
+{
+    class Dwarf : IRace
+    {
+        public Ability AbilityModifier => new Ability(constitution: 2);
+
+        public Size Size => Size.Medium;
+
+        public Alignment[] AlignmentRestriction => null;
+
+        public Proficiency[] Proficiencies => null;
+
+        public int Speed => 25;
+    }
+}
