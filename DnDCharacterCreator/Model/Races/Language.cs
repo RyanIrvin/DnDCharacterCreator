3105afeabe6c73e78eeea094cc5dd9d32c775e9a
RyanIrvin <40035854+RyanIrvin@users.noreply.github.com>
2020-01-12 18:04:24 -0500

    Add Language Enum

---
+++ DnDCharacterCreator/Model/Races/Language.cs
@@ -0,0 +1,28 @@
+﻿namespace DnDCharacterCreator.Model.Races
+{
+    public enum Language
+    {
+        Abyssal,
+        Aquan,
+        Auran,
+        Celestial,
+        Common,
+        DeepSpeech,
+        Draconic,
+        Druidic,
+        Dwarvish,
+        Elvish,
+        Giant,
+        Gnomish,
+        Goblin,
+        Gnoll,
+        Halfling,
+        Ignan,
+        Infernal,
+        Orc,
+        Primordial,
+        Sylvan,
+        Terran,
+        Undercommonn
+    }
+}
