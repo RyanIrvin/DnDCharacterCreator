2a8aff015b208d018b1d02f94cdbf5d60312396b
Ryan Irvin <rirvin@emoneyadvisor.com>
2020-01-13 16:42:10 -0500

    Add Human and Variant Human

---
+++ DnDCharacterCreator/Model/Races/Human/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDCharacterCreator.Model.Races
+{
+    class Human : IRace
+    {
+        public Ability AbilityModifier => new Ability(1,1,1,1,1,1);
+
+        public Size Size => Size.Medium;
+
+        public Alignment[] AlignmentRestriction => null;
+
+        public Proficiency[] Proficiencies => null;
+
+        public int Speed => 30;
+
+        public Language[] Languages => new Language[] { Language.Common }; //+1 other language
+    }
+}
---
+++ DnDCharacterCreator/Model/Races/Human/VariantHuman.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDCharacterCreator.Model.Races
+{
+    class VariantHuman: Human, ISubRace
+    {
+        //+1 to two different ability scores
+        private Ability Ability = new Ability(constitution: 1);
+
+        public new Ability AbilityModifier => Ability; //overrides base Human +1 to all stats
+
+    }
+}
